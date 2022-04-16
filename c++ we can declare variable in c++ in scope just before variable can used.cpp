c986ea54720aa7d32376a4de1801ce05253a51a8
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-04-16 19:56:04 +0530

    Add files via upload

---
+++ c++ we can declare variable in c++ in scope just before variable can used.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+const float pi=3.1452;
+int main()
+{
+	int r;
+	cout<<"\n enter radius";
+	cin>>r;
+	float area=pi*r*r;
+	cout<<"\n area="<<area;
+
+return 0;
+}
